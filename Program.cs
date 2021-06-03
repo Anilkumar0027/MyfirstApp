a1da407af7e0465d5f61ce1e99a5e91b8b6f231e
Anil Kumar <anilkumardumbu123@gmail.com>
2021-06-03 15:22:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MyfirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            String s;
+            char c;
+            Console.WriteLine("Enter the number");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the String");
+            s = Console.ReadLine();
+            Console.WriteLine("Enter the character");
+            c = Convert.ToChar(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
